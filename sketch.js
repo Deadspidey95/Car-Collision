a090817566075a8f07ca9ad6ec10fcbc24f5e28f
Deadspidey95 <69753764+Deadspidey95@users.noreply.github.com>
2020-09-19 19:36:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+
+  speed = random(55,90);
+  weight = random(400,1500);
+
+  car=createSprite(50,200,50,50);
+  car.velocityX = speed;
+
+  wall createSprite(1500,200,60,height/2);
+}
+
+var car, wall;
+
+var speed, weight;
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+
+  car.shapeColor = color(r,g,b);
+}
